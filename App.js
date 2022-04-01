6e1826eda4b8a932567552231a5c2a76620880f3
sorrow112 <jguirimahmed111@gmail.com>
2022-04-01 13:02:54 +0100

    more work on some functionalities

--- App.js
+++ App.js
@@ -6,10 +6,15 @@ const  io = require("socket.io")(8081,{
 io.on("connection", socket =>{
     console.log("call from"+socket.id)
     socket.on("join-rooms",rooms=>{
-        console.log("somebody joined room " + rooms)
         socket.join(rooms)
+        console.log("sombody joined " +rooms)
     })
+    socket.on("disconnect", ()=>console.log(socket.id + " got disconnected"))
     socket.on("AUGMENT", (enchere, user)=>{
         socket.to(enchere).emit("NEW_PRICE",enchere, user)
+        console.log("emit sent "+enchere )
+    })
+    socket.on("leave-room", enchere=>{socket.leave(enchere)
+    console.log("room left " + enchere)
     })
 })
